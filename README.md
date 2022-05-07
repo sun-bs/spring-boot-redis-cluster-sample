256f17c6eadf9ca3fdb5b2355e8849cc86e58436
TutorialUser <wt.kimura@gmail.com>
2022-05-07 15:56:16 +0900

    second commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# spring-boot-redis-cluster-sample
+## About
+This is sample of spring boot connected to ElastiCache redis cluster for spring session.
---
+++ src/main/java/com/example/springbootredisclustersample/MainController.java
@@ -0,0 +1,37 @@
+package com.example.springbootredisclustersample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+public class MainController {
+
+    private final HttpSession httpSession;
+
+    private final static String SESSION_KEY = "key";
+
+    @Autowired
+    public MainController(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+
+    @GetMapping("/set-session/{value}")
+    @ResponseBody
+    public String setValueToRedis(@PathVariable("value") String value) {
+        httpSession.setAttribute(SESSION_KEY, value);
+        return "OK";
+    }
+
+    @GetMapping("/get-session")
+    @ResponseBody
+    public String getValueFromRedis() {
+        String value = (String) httpSession.getAttribute(SESSION_KEY);
+        return value == null ? "none" : value;
+    }
+}
